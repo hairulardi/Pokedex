8bbb49f861203175c8ef833aae955ec04ae3eb09
Hairul Ardi <der.kaizer11@hotmail.com>
2022-04-09 21:04:18 +0700

    add infinite scroll

--- src/services/PokemonService.js
+++ src/services/PokemonService.js
@@ -5,20 +5,28 @@ import PokemonModel from '@/models/PokemonModel';
 const BASE_URL = HttpCOnfig.getBaseURL();
 
 export default {
-    getPokemonList(){
-        let url = `${BASE_URL}/pokemon/?offset=0&limit=50`;
+    getPokemonList(nextUrl = null){
+        let url = nextUrl ? nextUrl : `${BASE_URL}/pokemon`;
 
         return axios.get(url).then(response => {
             let data = response.data.results
+            let total = response.data.count
+            let next = response.data.next
 
             let promises = data.map(result => {
                 return axios.get(result.url)
             })
 
             return Promise.all(promises).then(response => {
-                return response.map(response => {
+                let data = response.map(response => {
                     return new PokemonModel(response.data);
                 })
+
+                return {
+                    total: total,
+                    next: next,
+                    data: data
+                }
             })
         });
     },
--- src/views/PokemonHome.vue
+++ src/views/PokemonHome.vue
@@ -1,29 +1,55 @@
 <template>
     <HeaderApp title="Pokedex"/>
 
-    <div class="page-content">
-        <PokemonListItem v-for="(item, index) of state.data" v-bind:key="index" v-bind:data="item" />
+    <div class="page-content" ref="scrollComponent">
+        <PokemonListItem v-for="(item, index) of pokemonData" v-bind:key="index" v-bind:data="item" />
     </div>
 </template>
 
 <script setup>
-    import { onMounted, reactive } from 'vue';
+    import { onMounted, onUnmounted, ref } from 'vue';
     import HeaderApp from '@/components/HeaderApp.vue';
     import PokemonListItem from '@/components/PokemonListItem.vue';
     import PokemonService from '@/services/PokemonService.js';
 
-    const state = reactive({ 
-        data: Array
-    })
+    const scrollComponent = ref(null);
+    const pokemonData = ref(null);
+    const onActiveLoad = ref(false);
+    const nextPage = ref(null);
+    const total = ref(0)
 
     onMounted(() => {
-        getPokemonList()
+        getPokemonList();
+        window.addEventListener("scroll", handleScroll);
     })
 
-    function getPokemonList() {
+    onUnmounted(() => {
+        window.removeEventListener("scroll", handleScroll)
+    })
+
+    const getPokemonList = () => {
         PokemonService.getPokemonList().then(response => {
-            // console.log(response);
-            state.data = response
+            pokemonData.value = response.data;
+            nextPage.value = response.next;
+            total.value = response.total;
         });
     }
+
+    const handleScroll = () => {
+        let element = scrollComponent.value;
+        let bottom = element.getBoundingClientRect().bottom
+
+        if ( (bottom < window.innerHeight) && !onActiveLoad.value && (pokemonData.value.length < total.value) ) {
+            onActiveLoad.value = true;
+            
+            PokemonService.getPokemonList(nextPage.value).then(response => {
+                pokemonData.value.push(...response.data);
+                nextPage.value = response.next;
+                onActiveLoad.value = false;
+
+                // console.log(pokemonData.value)
+            });
+        }
+    };
+
 </script>
